b15851aa79e3491840c06acb663467e37bdb62e5
Marie <mariesophie@users.noreply.github.com>
2016-08-14 09:56:15 +0200

    basic functionality

---
+++ GPIO-morse.cpp
@@ -0,0 +1,36 @@
+#include <wiringPi.h>
+
+// include standard library
+#include <stdio.h>
+
+int main() {
+
+	// start WiringPi Api (very important)
+	if (wiringPiSetup() == -1)
+		return 1;
+
+	int intPin = 0;
+	int intDit = 100; // time in ms
+	int intDah = 3 * intDit;
+	int intGapInterElement = intDit;
+	int intGapLetter = intDah;
+	int intGapWord = 7 * intDit;
+
+	// set GPIO 17 (=WiringPi Pin 0) to output
+	// Note: WiringPi Layout is used (http://wiringpi.com/pins/)
+	pinMode(intPin, OUTPUT);
+
+	// endless loop
+	// send A
+	while (1) {
+		digitalWrite(intPin, 1);	// LED on
+		delay(intDit);				// .
+		digitalWrite(intPin, 0);	// LED off
+		delay(intGapInterElement)	// pause between . and -
+		digitalWrite(intPin, 1);	// LED on
+		delay(intDah);				// -
+		digitalWrite(intPin, 0);	// LED off
+		delay(intGapWord);			// end of letter and even word
+	}
+	return 0;
+}
